0bafab58bf0a8cd85c948f775138491f0810b2eb
yanlifeng <yanlifeng19981116@qq.com>
2022-04-22 23:28:26 +0800

    fix pugz bug, now xx_pigz_9_t32.fq.gz is okk

--- lib/gzip_decompress.hpp
+++ lib/gzip_decompress.hpp
@@ -41,91 +41,111 @@
 
 template<typename Consumer>
 static enum libdeflate_result
-libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, Consumer& consumer, ConsumerSync* sync)
-{
+libdeflate_gzip_decompress(const byte *in, size_t in_nbytes, unsigned nthreads, Consumer &consumer,
+                           ConsumerSync *sync) {
     // FIXME: handle header parsing inside DeflateThread*, allowing multimember gzip files
     InputStream in_stream2(in, in_nbytes);
     in_stream2.consume_header();
     InputStream in_stream(in_stream2.in_next, in_stream2.available());
-    size_t      in_size = in_stream2.available();
-    nthreads            = std::min(1 + unsigned(in_size >> 21), nthreads);
+    size_t in_size = in_stream2.available();
+    //TODO GG when gz file is not big
+    //nthreads = std::min(1 + unsigned(in_size >> 25), nthreads);
+    nthreads = std::min(1 + unsigned(in_size >> 21), nthreads);
 
     PRINT_DEBUG("Using %u threads\n", nthreads);
 
-//    printf("use %d threads\n", nthreads);
-//    std::cout << "mmap size " << in_nbytes << std::endl;
-//    std::cout << "in size " << in_size << std::endl;
+    std::vector<std::thread> threads;
+    std::vector<DeflateThread *> deflate_threads(nthreads);
 
-    std::vector<std::thread>    threads;
-    std::vector<DeflateThread*> deflate_threads(nthreads);
+    std::atomic<size_t> nready = {0};
+    //std::vector<std::atomic<size_t>> nreadys;
 
-    std::atomic<size_t>     nready = {0};
     std::condition_variable ready;
-    std::mutex              ready_mtx;
-    std::exception_ptr      exception;
+    std::mutex ready_mtx;
+    std::exception_ptr exception;
 
     threads.reserve(nthreads);
 
     // Sections of file decompressed sequentially
     size_t max_section_size = nthreads * (32ull << 20); // 32MB per thread
-    size_t section_size     = std::min(max_section_size, in_size);
-    size_t n_sections       = (in_size + section_size - 1) / section_size;
-
-
+    size_t section_size = std::min(max_section_size, in_size);
+    size_t n_sections = (in_size + section_size - 1) / section_size;
     if (nthreads == 1) n_sections = 1;
-//    std::cout << "n_sections " << n_sections << std::endl;
-
     section_size = in_size / n_sections;
-//    std::cout << "section_size " << section_size << std::endl;
 
     // Section are chunked to nethreads
     size_t chunk_size = section_size / nthreads;
     // The first thread is working with resolved context so its faster
     size_t first_chunk_size = chunk_size + (4UL << 20); // FIXME: ratio instead of delta
     if (nthreads > 1) {
-        chunk_size       = (nthreads * chunk_size - first_chunk_size) / (nthreads - 1);
+        chunk_size = (nthreads * chunk_size - first_chunk_size) / (nthreads - 1);
         first_chunk_size = section_size - chunk_size * (nthreads - 1);
     }
-//    std::cout << "chunk_size " << chunk_size << std::endl;
-//    std::cout << "first_chunk_size " << first_chunk_size << std::endl;
+    PRINT_DEBUG("in_size %zu\n", in_size);
+    PRINT_DEBUG("n_sections %zu\n", n_sections);
+    PRINT_DEBUG("section_size %zu\n", section_size);
+    PRINT_DEBUG("nthreads %zu\n", nthreads);
+    PRINT_DEBUG("chunk_size %zu\n", chunk_size);
+    PRINT_DEBUG("first_chunk_size %zu\n", first_chunk_size);
+    //nreadys.reserve(n_sections);
+    std::atomic<size_t> *nreadys=new std::atomic<size_t>[n_sections];
+    for (unsigned i = 0; i < n_sections; i++) {
+        nreadys[i] = 0;
+    }
 
     for (unsigned chunk_idx = 0; chunk_idx < nthreads; chunk_idx++) {
         if (chunk_idx == 0) {
-
-            threads.emplace_back([&]() {
+            threads.emplace_back([&, chunk_idx]() {
                 ConsumerWrapper<Consumer> consumer_wrapper{consumer, sync};
                 consumer_wrapper.set_chunk_idx(0, nthreads == 1);
                 DeflateThread deflate_thread(in_stream, consumer_wrapper);
-                PRINT_DEBUG("chunk 0 is %p\n", (void*)&deflate_thread);
+                deflate_thread.threadNum = chunk_idx;
 
+                PRINT_DEBUG("chunk 0 is %p\n", (void *) &deflate_thread);
                 {
                     std::unique_lock<std::mutex> lock{ready_mtx};
                     deflate_threads[0] = &deflate_thread;
                     nready++;
                     ready.notify_all();
-
                     while (nready != nthreads)
                         ready.wait(lock);
+                    PRINT_DEBUG("thread %lu init sync\n", chunk_idx);
+                    //! thread sync
+                    if (chunk_idx < nthreads - 1)
+                        deflate_thread.set_downstream(deflate_threads[chunk_idx + 1]);
                 }
-                PRINT_DEBUG("chunk 0 is pre ok\n");
+                //usleep(100);
 
                 try {
-                    const byte* last_unmapped = details::round_up<details::huge_page_size>(in);
+                    {
+                        std::unique_lock<std::mutex> lock{ready_mtx};
+                        nreadys[0]++;
+                        ready.notify_all();
+                        while (nreadys[0] != nthreads)
+                            ready.wait(lock);
+                        PRINT_DEBUG("thread %lu sync on section %lu\n", chunk_idx, 0);
+                    }
+                    //usleep(100);
+                    const byte *last_unmapped = details::round_up<details::huge_page_size>(in);
 
                     // First chunk of first section: no context needed
-                    deflate_thread.set_end_block(first_chunk_size * 8);
-                    PRINT_DEBUG("chunk 0 set_end_block ok\n");
-
+                    if (chunk_idx == nthreads - 1)
+                        deflate_thread.set_end_block(first_chunk_size * 8);
                     deflate_thread.go(0);
-                    PRINT_DEBUG("chunk 0 go ok\n");
-
 
                     // First chunks of next sections get their contexts from the last chunk of the previous section
-                    auto& prev_chunk = *deflate_threads[nthreads - 1];
+                    auto &prev_chunk = *deflate_threads[nthreads - 1];
                     for (unsigned section_idx = 1; section_idx < n_sections && nready == nthreads; section_idx++) {
-
+                        {
+                            std::unique_lock<std::mutex> lock{ready_mtx};
+                            nreadys[section_idx]++;
+                            ready.notify_all();
+                            while (nreadys[section_idx] != nthreads)
+                                ready.wait(lock);
+                            PRINT_DEBUG("thread %lu sync on section %lu\n", chunk_idx, section_idx);
+                        }
                         const size_t start = section_idx * section_size;
-                        const size_t stop  = start + first_chunk_size;
+                        const size_t stop = start + first_chunk_size;
 
                         // Get the context and position of the first block of the section
                         size_t resume_bitpos;
@@ -135,23 +155,32 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
                             resume_bitpos = ctx.second;
                         }
 
+                        //! ?
                         // Unmmap the section previously decompressed (free RSS, usefull for large files)
-                        const byte* unmap_end
-                          = details::round_down<details::huge_page_size>(in_stream.data.begin() + resume_bitpos / 8);
-                        sys::check_ret(munmap(const_cast<byte*>(last_unmapped), size_t(unmap_end - last_unmapped)),
+                        const byte *unmap_end
+                                = details::round_down<details::huge_page_size>(
+                                        in_stream.data.begin() + resume_bitpos / 8);
+                        sys::check_ret(munmap(const_cast<byte *>(last_unmapped), size_t(unmap_end - last_unmapped)),
                                        "munmap");
                         last_unmapped = unmap_end;
+                        //! ?
 
-                        PRINT_DEBUG("%p chunk 0 of section %u: [%lu<=%lu, %lu[\n",
-                                    (void*)&deflate_thread,
+
+                        PRINT_DEBUG("=====%p chunk 0 of section %u: [%lu<=%lu, %lu]\n",
+                                    (void *) &deflate_thread,
                                     section_idx,
                                     start * 8,
                                     resume_bitpos,
-                                    stop);
+                                    stop * 8);
 
                         assert(resume_bitpos >= start * 8);
                         assert(resume_bitpos < stop * 8);
-                        deflate_thread.set_end_block(stop * 8);
+                        //! ylf fix0
+                        //! all add a condition before set block end, actually in sync(),
+                        //! all stream a real block end from its down stream, except the last one
+                        //! and, if set here, maybe overflow the real block value then get error
+                        if (chunk_idx == nthreads - 1)
+                            deflate_thread.set_end_block(stop * 8);
                         consumer_wrapper.set_section_idx(section_idx);
                         deflate_thread.go(resume_bitpos);
                     }
@@ -160,7 +189,9 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
                     std::unique_lock<std::mutex> lock{ready_mtx};
                     if (!exception) {
                         exception = std::current_exception();
-                        nready    = 0; // Stop the thread pool
+                        nready = 0; // Stop the thread pool
+                        for (unsigned i = 0; i < n_sections; i++)
+                            nreadys[i] = 0;
                     }
                     return;
                 }
@@ -172,8 +203,10 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
                 ConsumerWrapper<Consumer> consumer_wrapper{consumer, sync};
                 consumer_wrapper.set_chunk_idx(chunk_idx, chunk_idx == nthreads - 1);
                 DeflateThreadRandomAccess deflate_thread{in_stream, consumer_wrapper};
-                PRINT_DEBUG("chunk %u is %p\n", chunk_idx, (void*)&deflate_thread);
+                deflate_thread.threadNum = chunk_idx;
+                PRINT_DEBUG("=====chunk %u is %p\n", chunk_idx, (void *) &deflate_thread);
                 {
+                    //! init all deflate_thread
                     std::unique_lock<std::mutex> lock{ready_mtx};
                     deflate_threads[chunk_idx] = &deflate_thread;
                     nready++;
@@ -181,29 +214,46 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
 
                     while (nready != nthreads)
                         ready.wait(lock);
-
+                    //! thread sync
+                    PRINT_DEBUG("thread %lu init sync\n", chunk_idx);
+                    if (chunk_idx < nthreads - 1)
+                        deflate_thread.set_downstream(deflate_threads[chunk_idx + 1]);
                     deflate_thread.set_upstream(deflate_threads[chunk_idx - 1]);
                 }
-
-                PRINT_DEBUG("chunk %u is pre ok\n", chunk_idx);
+                //usleep(100);
 
                 try {
                     const size_t chunk_offset_start = first_chunk_size + chunk_size * (chunk_idx - 1);
-                    const size_t chunk_offset_stop  = first_chunk_size + chunk_size * chunk_idx;
+                    const size_t chunk_offset_stop = first_chunk_size + chunk_size * chunk_idx;
+
 
                     for (unsigned section_idx = 0; section_idx < n_sections && nready == nthreads; section_idx++) {
+                        {
+                            std::unique_lock<std::mutex> lock{ready_mtx};
+                            nreadys[section_idx]++;
+                            ready.notify_all();
+                            while (nreadys[section_idx] != nthreads)
+                                ready.wait(lock);
+                            PRINT_DEBUG("thread %lu sync on section %lu\n", chunk_idx, section_idx);
+                        }
+                        //usleep(100);
                         const size_t section_offset = section_idx * section_size;
-                        const size_t start          = section_offset + chunk_offset_start;
-                        const size_t stop           = section_offset + chunk_offset_stop;
+                        const size_t start = section_offset + chunk_offset_start;
+                        const size_t stop = section_offset + chunk_offset_stop;
 
-                        PRINT_DEBUG("%p chunk %u of section %u [%lu, %lu[\n",
-                                    (void*)&deflate_thread,
+                        //! roughly divided by size
+                        PRINT_DEBUG("=====%p chunk %u of section %u [%lu, %lu]\n",
+                                    (void *) &deflate_thread,
                                     chunk_idx,
                                     section_idx,
                                     start * 8,
                                     stop * 8);
-                        deflate_thread.set_end_block(stop * 8);
+                        //! set block end roughly
+                        if (chunk_idx == nthreads - 1)
+                            deflate_thread.set_end_block(stop * 8);
+
                         consumer_wrapper.set_section_idx(section_idx);
+                        //! main process
                         if (!deflate_thread.go(start * 8)) return;
                         assert(chunk_idx != nthreads - 1 || stop == section_offset + section_size);
                     }
@@ -216,7 +266,9 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
                     std::unique_lock<std::mutex> lock{ready_mtx};
                     if (!exception) {
                         exception = std::current_exception();
-                        nready    = 0; // Stop the thread pool
+                        nready = 0; // Stop the thread pool
+                        for (unsigned i = 0; i < n_sections; i++)
+                            nreadys[i] = 0;
                     }
                     return;
                 }
@@ -224,7 +276,7 @@ libdeflate_gzip_decompress(const byte* in, size_t in_nbytes, unsigned nthreads, 
         }
     }
 
-    for (auto& thread : threads)
+    for (auto &thread: threads)
         thread.join();
 
     if (exception) { std::rethrow_exception(exception); }
